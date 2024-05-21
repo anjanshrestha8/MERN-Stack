83b3cbf6cbc60d727a5325b5f660dd940259e88e
anjanshrestha8 <anjanshrestha2023@gmail.com>
2024-05-21 17:49:41 +0545

    task added

--- Use-Effect-Learn/src/App.jsx
+++ Use-Effect-Learn/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useEffect, useState } from "react";
+import moment from "moment";
+import "./App.css";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
+  const today = Date.now();
+  // console.log(moment(today).format("MMMM Do YYYY, h:mm:ss a"));
+  console.log(moment().fromNow());
+
+  useEffect(() => {
+    console.log(input);
+  }, [input]);
+
+  useEffect(() => {
+    console.log("counter ko ho yo ");
+  }, [count]);
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <span>Current Count is :{count}</span>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <button
+        onClick={() => {
+          setCount((prev) => prev + 1);
+        }}
+      >
+        Counter increase
+      </button>
+
+      <div>
+        <input
+          type="text"
+          onChange={(event) => {
+            setInput(event.target.value);
+          }}
+          placeholder="type something"
+        />
+        <p>{input}</p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
